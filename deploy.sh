5c666a15345edb56782de4b930aae2fe079cd071
Jeshua Jemson <jeshuamj2@gmail.com>
2015-12-29 11:50:09 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 bundle exec middleman build
-cd buildinit
-git remote add origin git@github.com:jeshuamj2/jeshuamj2.github.io.git;
-git add --all
-git commit -m "Build pushed on `date`"
+cd build
+git init &&
+git remote add origin git@github.com:jeshuamj2/jeshuamj2.github.io.git &&
+git add --all &&
+git commit -m "Build pushed on `date`" &&
 git push --force
