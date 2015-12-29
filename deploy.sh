d08e76a7a8d67449ea9ed5a8eafc245ed664c5bc
Jeshua Jemson <jeshuamj2@gmail.com>
2015-12-29 11:40:27 -0500

    Build pushed on Tue Dec 29 11:40:27 EST 2015

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 bundle exec middleman build
-(cd build;
+pushd build;
 git remote add origin git@github.com:jeshuamj2/jeshuamj2.github.io.git;
 git add --all;
 git commit -m "Build pushed on `date`";
 git push --force;
-)
+popd
